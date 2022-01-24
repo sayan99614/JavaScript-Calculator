72568a8929b689bcbc24b9edacfb78776f8bb0cc
dheeman pati <dheemanpati53@gmail.com>
2022-01-24 13:56:00 +0530

    modified some logic part

--- js/main.js
+++ js/main.js
@@ -371,7 +371,7 @@ function calculator(btn) {
   } else if (btn.type === "math_function") {
     let symbol, formula;
     if (btn.name == "factorial") {
-      symbol = "!(";
+      symbol = "!";
       formula = btn.formula;
       data.operation.push(symbol);
       data.formula.push(formula);
@@ -447,28 +447,37 @@ function calculator(btn) {
       console.log("clicked");
       updateOutput(data.operation.join(""));
     } else if (btn.name == "plusorminys") {
-        data.operation[pointer] = "-" + data.operation[pointer];
-        data.formula[pointer] = "-" + data.formula[pointer];
-        updateOutput(data.operation.join(""));
+      data.operation[pointer] = "-" + data.operation[pointer];
+      data.formula[pointer] = "-" + data.formula[pointer];
+      updateOutput(data.operation.join(""));
     }
   } else if (btn.type === "calculate") {
     let formula_str = data.formula.join("");
-    let POWER_SEARCH_RESULT=search(data.formula,POWER);
-    let FACTORIAL_SEARCH_RESULT=search(data.formula,FACTORIAL);
+    let POWER_SEARCH_RESULT = search(data.formula, POWER);
+    let FACTORIAL_SEARCH_RESULT = search(data.formula, FACTORIAL);
 
-    const BASES=powerBaseGetter(data.formula,POWER_SEARCH_RESULT);
+    const BASES = powerBaseGetter(data.formula, POWER_SEARCH_RESULT);
+    const NUMBERS = factorialNumberGetter(
+      data.formula,
+      FACTORIAL_SEARCH_RESULT
+    );
 
-    BASES.forEach(base=>{
-      let toReplace=base+POWER;
-      let replacement="Math.pow("+base+",";
-      formula_str=formula_str.replace(toReplace,replacement);
+    BASES.forEach((base) => {
+      let toReplace = base + POWER;
+      let replacement = "Math.pow(" + base + ",";
+      formula_str = formula_str.replace(toReplace, replacement);
+    });
+
+    NUMBERS.forEach(factorial=>{
+      formula_str=formula_str.replace(factorial.toReplace,factorial.replacement);
     })
+
     let result;
     try {
-        result = eval(formula_str);
+      result = eval(formula_str);
     } catch (error) {
-      if(error instanceof SyntaxError){
-        result="Syntax Error!";
+      if (error instanceof SyntaxError) {
+        result = "Syntax Error!";
         updateOutput(result);
         return;
       }
@@ -485,98 +494,151 @@ function calculator(btn) {
   }
 }
 
-function search(aray,keyword){
-  let search_result=[];
-  aray.forEach((element,index)=>{
-    if(element==keyword){
+function search(aray, keyword) {
+  let search_result = [];
+  aray.forEach((element, index) => {
+    if (element == keyword) {
       search_result.push(index);
     }
-  })
+  });
   return search_result;
 }
-function powerBaseGetter(formula,POWER_SEARCH_RESULT){
-  let power_bases=[];
-  POWER_SEARCH_RESULT.forEach(power_index=>{
-    let base=[];
-    let parenthesis_count=0;
-    let previous_index=power_index-1;
-    while(previous_index >=0 ){
-        if(formula[previous_index]=="(") parenthesis_count--;
-        if(formula[previous_index]==")") parenthesis_count++;
 
-        let is_operator=false;
-        operators.forEach(OPERATOR=>{
-          if(formula[previous_index]==OPERATOR) is_operator=true;
-        });
+function factorialNumberGetter(formula, FACTORIAL_SEARCH_RESULT) {
+  let numbers = [];
+  let factorial_sequence = 0;
+  FACTORIAL_SEARCH_RESULT.forEach((factorial_index) => {
+    let number = [];
 
-        let is_power=formula[previous_index]==POWER;
+    let next_index = factorial_index + 1;
+    let next_input = formula[next_index];
 
-        if((is_operator && parenthesis_count==0)||is_power) break;
+    if (next_input == FACTORIAL) {
+      factorial_sequence += 1;
+      return;
+    }
+    let first_factorial_index = factorial_index - factorial_sequence;
+    let previous_index = first_factorial_index - 1;
 
-        base.unshift(formula[previous_index]);
-        previous_index--;
+    let parenthesis_count = 0;
+
+    while (previous_index >= 0) {
+      if (formula[previous_index] == "(") parenthesis_count--;
+      if (formula[previous_index] == ")") parenthesis_count++;
+
+      let is_operator = false;
+      operators.forEach((OPERATOR) => {
+        if (formula[previous_index] == OPERATOR) is_operator = true;
+      });
+
+
+      if ((is_operator && parenthesis_count == 0)) break;
+
+      number.unshift(formula[previous_index]);
+      previous_index--;
+    }
+  let number_str=number.join('');
+  const factorial="factorial(",close_parenthesis=")";
+  let times=factorial_sequence+1;
+  let toReplace=number_str+FACTORIAL.repeat(times);
+  let replacement=factorial.repeat(times)+number_str+close_parenthesis.repeat(times);    
+  
+  numbers.push({
+    toReplace:toReplace,
+    replacement:replacement
+  })
+
+  factorial_sequence=0;
+  });
+  return numbers;
+}
+
+function powerBaseGetter(formula, POWER_SEARCH_RESULT) {
+  let power_bases = [];
+  POWER_SEARCH_RESULT.forEach((power_index) => {
+    let base = [];
+    let parenthesis_count = 0;
+    let previous_index = power_index - 1;
+    while (previous_index >= 0) {
+      if (formula[previous_index] == "(") parenthesis_count--;
+      if (formula[previous_index] == ")") parenthesis_count++;
+
+      let is_operator = false;
+      operators.forEach((OPERATOR) => {
+        if (formula[previous_index] == OPERATOR) is_operator = true;
+      });
+
+      let is_power = formula[previous_index] == POWER;
+
+      if ((is_operator && parenthesis_count == 0) || is_power) break;
+
+      base.unshift(formula[previous_index]);
+      previous_index--;
     }
     power_bases.push(base.join(""));
-  })
+  });
   return power_bases;
 }
 
 function updateOutput(operation) {
   display.value = operation;
 }
-let deg=true;
-document.getElementById("deg").addEventListener("click",()=>{
-  if(document.getElementById("deg").value=="DEG"){
-    document.getElementById("deg").value="RED";
-    deg=!deg;
-  }else{
-    document.getElementById("deg").value="DEG";
-    deg=!deg;
+let deg = true;
+document.getElementById("deg").addEventListener("click", () => {
+  if (document.getElementById("deg").value == "DEG") {
+    document.getElementById("deg").value = "RED";
+    deg = !deg;
+  } else {
+    document.getElementById("deg").value = "DEG";
+    deg = !deg;
   }
   console.log(deg);
 });
 
-function trigo(callback,angle){
-  if(!deg){
-    angle=angle*Math.PI/100;
+function trigo(callback, angle) {
+  if (!deg) {
+    angle = (angle * Math.PI) / 100;
   }
   return callback(angle);
 }
 
-function inv_trigo(callback,value){
-  let angle=callback(value);
-  if(!deg){
-    angle=angle*180/Math.PI;
+function inv_trigo(callback, value) {
+  let angle = callback(value);
+  if (!deg) {
+    angle = (angle * 180) / Math.PI;
   }
   return angle;
 }
 
-function factorial(n){
-    if(n%1!=0){
-      return gamma(n);
-    }
-    if(n==0 || n==1){
-      return 1;
-    }
-    return n*factorial(n-1);
+function factorial(n) {
+  if (n % 1 != 0) {
+    return gamma(n);
+  }
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 // GAMMA FUNCTINON
-function gamma(n) {  // accurate to about 15 decimal places
-  //some magic constants 
+function gamma(n) {
+  // accurate to about 15 decimal places
+  //some magic constants
   var g = 7, // g represents the precision desired, p is the values of p[i] to plug into Lanczos' formula
-      p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
-  if(n < 0.5) {
+    p = [
+      0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+      771.32342877765313, -176.61502916214059, 12.507343278686905,
+      -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7,
+    ];
+  if (n < 0.5) {
     return Math.PI / Math.sin(n * Math.PI) / gamma(1 - n);
-  }
-  else {
+  } else {
     n--;
     var x = p[0];
-    for(var i = 1; i < g + 2; i++) {
+    for (var i = 1; i < g + 2; i++) {
       x += p[i] / (n + i);
     }
     var t = n + g + 0.5;
-    return Math.sqrt(2 * Math.PI) * Math.pow(t, (n + 0.5)) * Math.exp(-t) * x;
+    return Math.sqrt(2 * Math.PI) * Math.pow(t, n + 0.5) * Math.exp(-t) * x;
   }
 }
-
